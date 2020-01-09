400f03911778dbad7ec77e3d354770368ac2be8b
Rukhsar Khan <53951120+laraib-786@users.noreply.github.com>
2020-01-09 23:29:27 +0530

    Update testbench_frequency_counter.v

--- testbench_frequency_counter.v
+++ testbench_frequency_counter.v
@@ -1,26 +1,3 @@
-//`timescale 10 ms / 1 ms
-//module testbench_frequency_counter;
-//reg vol, reset, clear;
-//wire [5:0] q;
-//frequency_counter q1(vol, reset, clear, q);
-//initial 
-//$monitor($time, "vol = %b, reset = %b, q = %b", vol, reset, q);
-//initial
-////begin
-//reset = 1'b1;
-//#2 reset = 1'b0;                                                  
-//end
-//initial
-//clear = 1'b0;
-//initial
-//begin
-//vol = 1'b0;
-//forever #1 vol = ~vol;
-//end
-//initial
-//#100 $stop;
-//endmodule
-
 `timescale 10 ms / 1 ms
 module testbench_frequency_counter;
 reg vol ,reset;
